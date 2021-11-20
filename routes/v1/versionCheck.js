7e7ed0717a88bd8df0313561d21abf20e4b75b55
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-11-19 22:26:50 -0700

    Update versionCheck.js

--- routes/v1/versionCheck.js
+++ routes/v1/versionCheck.js
@@ -1,4 +1,4 @@
-const landingPage = (fastify, options, done) => {
+const versionCheck = (fastify, options, done) => {
     
     fastify.get("/v1/versions/check", async (req, res) => {
 
@@ -20,4 +20,4 @@ const landingPage = (fastify, options, done) => {
     done()
   };
 
-module.exports = landingPage;
+module.exports = versionCheck;
