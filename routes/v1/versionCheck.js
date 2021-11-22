5a47eaecf2d8b8fe97a6a687c334afec73f56b33
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-11-22 16:06:24 -0700

    Update versionCheck.js

--- routes/v1/versionCheck.js
+++ routes/v1/versionCheck.js
@@ -4,9 +4,9 @@ const versionCheck = (fastify, options, done) => {
 
         res.header("Content-Type", "application/json");
 
-        let current_version = '2.1.2'
-        let previous_version = '2.1.1'
-        let newest_version = '2.1.2'
+        let current_version = '2.1.3'
+        let previous_version = '2.1.2'
+        let newest_version = '2.1.3'
         let stable_version = '2.1.2'
 
         res.status(200).send(JSON.stringify({
