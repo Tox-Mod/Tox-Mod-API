3d6f33dc9ec49717ba68c07101b9d3703b426681
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-11-23 04:40:16 -0700

    Update voteHook.js

--- routes/v1/voteHook.js
+++ routes/v1/voteHook.js
@@ -1,12 +1,28 @@
+const { MessageEmbed } = require('discord.js');
+const config = require("../config.js");
+const Infinity = require('infinity-bots');
+
+const webhook = new Infinity.Webhook('ToxicIsBae123')
+
 const voteHook = (fastify, options, done) => {
     
-    fastify.post("/hooks/vote", async (req, res) => {
+    fastify.post("/hooks/vote", webhook.hookListener(async (vote, req, res) => {
 
         res.header("authorization", "ToxicIsBae123");
 
-        console.log(vote.user)
+        let client = req.client
+
+        let voteLog = new MessageEmbed()
+          .setTitle('⬆️ Vote Logs')
+          .setColor('#0EFF00')
+          .setThumbnail('https://toxmod.xyz/images/ToxModLogo.gif')
+          .setDescription(`<@!${vote.user}> has gave me a Upvote on Infinity`)
+          .setTimestamp()
+          .setFooter('© 2021 Tox Mod', 'https://toxmod.xyz/images/ToxModLogo.gif')
+            
+        await client.guilds.cache.get(config.guildID).channels.cache.get(config.logsID).send({ embeds: [voteLog] }); 
        
-    });
+    }));
 
     done()
   };
