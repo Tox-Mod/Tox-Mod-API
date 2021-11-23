8bf766da563b146a3348c3b58f1c9549049f20dd
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-11-23 05:23:25 -0700

    Update voteHook.js

--- routes/v1/voteHook.js
+++ routes/v1/voteHook.js
@@ -9,12 +9,16 @@ const voteHook = (fastify, options, done) => {
     fastify.post("/v1/hooks/vote", webhook.hookListener(async (vote, req, res) => {
 
         let client = req.client
+        
+        let user = client.users.cache.get(vote.userID) || vote.userID
 
         let voteLog = new MessageEmbed()
           .setTitle('⬆️ UpVote Logs')
           .setColor('#0EFF00')
           .setThumbnail('https://toxmod.xyz/images/ToxModLogo.gif')
-          .setDescription(`<@!${vote.userID}> has gave me a Upvote on Infinity`)
+          .setDescription(`Somone has voted for me on [Infinity Bot List](https://infinitybotlist.com/bots/631558023109804032/vote)`)
+          .addField('User', `<@!${user}>`, true)
+          .addField('User ID', `${vote.userID}`, true
           .setTimestamp()
           .setFooter('© 2021 Tox Mod', 'https://toxmod.xyz/images/ToxModLogo.gif')
             
