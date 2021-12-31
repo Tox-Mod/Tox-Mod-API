a938d047a657aac7902d8af9a15dbdedab52555c
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-12-31 11:33:58 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    token: "NjMxNTU4MDIzMTA5ODA0MDMy.XZ4mPg.sX6iBia_070GXYbl_mxyREUyUC8",
-    mongoose: "mongodb://admin:eMrFan00MqjT4OFN@SG-ToxMod-40838.servers.mongodirector.com:27017/admin",
+    token: process.env.TOKEN,
+    mongoose: process.env.MONGO,
     port: process.env.PORT,
     domain: "https://api.toxmod.xyz",
-    webhook_id:"911177425168252968", 
+    webhook_id: process.env.hookID,
     webhook_url:"rKAHFnxghzbILa5lSnAbLe9lT22e23QXafSyYNrKUQsObLTK2Q3nympjpC4QaIDEasxY",
     guildID: '896849194290130985',
     logsID: '911175702248820796',
