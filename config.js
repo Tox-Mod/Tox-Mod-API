32b482aba2f3df7f2bff73b5b1335a47c6e456e7
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-12-31 11:35:02 -0700

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ module.exports = {
     port: process.env.PORT,
     domain: "https://api.toxmod.xyz",
     webhook_id: process.env.hookID,
-    webhook_url:"rKAHFnxghzbILa5lSnAbLe9lT22e23QXafSyYNrKUQsObLTK2Q3nympjpC4QaIDEasxY",
-    guildID: '896849194290130985',
+    webhook_url:"hookURL",
+    guildID: process.env.guildID,
     logsID: '911175702248820796',
     voteLogs: '912675970585088042',
 }
